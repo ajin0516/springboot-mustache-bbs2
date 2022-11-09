2f8c0474b644c791b842fa40cf7d96068d6ee1f8
ajin0516 <dkwls7432@naver.com>
2022-11-09 15:05:31 +0900

    [mustache] Edit, Update

--- src/main/java/com/mustache/bbs2/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs2/controller/ArticleController.java
@@ -3,14 +3,10 @@ package com.mustache.bbs2.controller;
 import com.mustache.bbs2.domain.entity.Article;
 import com.mustache.bbs2.domain.dto.ArticleDto;
 import com.mustache.bbs2.repository.ArticleRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,12 +28,12 @@ public class ArticleController {
     public String list(Model model){
         List<Article> articles = articleRepository.findAll();
         model.addAttribute("articles", articles);
-        return "list";
+        return "articles/list";
     }
 
     @GetMapping("")
     public String index(){
-        return "redirect:/list";
+        return "redirect:/articles/list";
     }
 
     @GetMapping("/new")
@@ -45,6 +41,18 @@ public class ArticleController {
         return "articles/new";
     }
 
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable Long id, ArticleDto articleDto, Model model){
+        log.info("title:{} content:{}", articleDto.getTitle(), articleDto.getContent());
+        Article article = articleRepository.save(articleDto.toEntity());
+        // Duplicated Entry에러가 안나는 이유 : update쿼리 실행
+        // id가 있는 경우: update   id가 없는 경우: insert
+        model.addAttribute("article", article);
+        return String.format("redirect:/articles/%d", article.getId());
+    }
+
+    // addAttribute view객체를 name이름으로 추가
     @GetMapping("/{id}")
     public String selectSingle(@PathVariable Long id, Model model){
         Optional<Article> optionalArticle = articleRepository.findById(id);
@@ -52,6 +60,17 @@ public class ArticleController {
             model.addAttribute("article", optionalArticle.get());
             return "articles/show";
         }else {
+            return "articles/error";
+        }
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if(!optionalArticle.isEmpty()) {
+            model.addAttribute("article", optionalArticle.get());
+            return "articles/edit";
+        }else {
+            model.addAttribute("message", String.format("%d가 없습니다",id));
             return "articles/error";
         }
     }
--- src/main/java/com/mustache/bbs2/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs2/domain/entity/Article.java
@@ -1,5 +1,6 @@
 package com.mustache.bbs2.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Table(name = "article2")  // 테이블 명 지정
 @Getter
 @NoArgsConstructor  // 생성자 추가
+@AllArgsConstructor
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // db에 아이디생성 맡기겠다, primary키
